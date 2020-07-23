dc9296070837ec68418c57235e904c7e3b4f830c
malorento <68697283+malorento@users.noreply.github.com>
2020-07-23 16:55:10 +0300

    Add files via upload

---
+++ Core/decor.py
@@ -0,0 +1,37 @@
+from colorama import Fore
+
+res = Fore.RESET
+lye = Fore.LIGHTYELLOW_EX
+lre = Fore.LIGHTRED_EX
+
+
+menu = f"""
+{lye}
+[1] - Found Nick Information
+[2] - Find Torrent Information
+[3] - Find Phone or Car Number Information
+{lre}[4] - Exit {res}
+"""
+
+chsystem = f"""
+{lre}[!]Choose Your System[!]{lye}
+[1] - Windows
+[2] - Linux/Termux{res}
+"""
+
+banner = f""" {lye}
+   _____ _     _
+  / ____(_)   | |
+ | |  __ _  __| | ___  ___  _ __
+ | | |_ | |/ _` |/ _ \/ _ \| '_ \\
+ | |__| | | (_| |  __/ (_) | | | |
+  \_____|_|\__,_|\___|\___/|_| |_|
+  {lye}[>>] created by {lre}malorento
+  {lye}[>>] for channels {lre}"You`ve been HACKED" {lye}and{lre} "staide"
+  {lye}[>>] the idea belongs to {lre}consulol{res}
+"""
+
+sher_menu = f"""{lye}
+[1] - Phone Information
+[2] - Car Information{res}
+"""
---
+++ README.md
@@ -0,0 +1,25 @@
+# Gideon 
+🧥🔍Simple tool for searching and collect information written in Python 3
+
+💰My btc for donations: 1Ak3xRuLXh5cex3kb53idPNv2jdnpuVRJ2
+
+# Functions:
+1. Search Information about Phone Number
+2. Seaarch Information about Car Number
+3. Search Nickname in the Internet
+4. Search all downloaded Torrents by IP
+
+# Installation:
+``git clone https://github.com/malorento/gideon/``
+
+``cd gideon``
+
+``pip3 install requirements.txt``
+
+``python gideon.py``
+
+# About:
+1. Created by malorento
+2. Owned by malorento
+3. Copy without copyrightings will burn your soul
+4. The idea of ``gideon`` is belongs to consulol
